83c9025036c9ae154f32bf1daf40b9e2783b867d
Rubén Waldo <147012926+Konekt4@users.noreply.github.com>
2024-07-04 01:35:32 -0600

    Funcionando

--- log_visits.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Registrar la visita
-$file = 'visitas.txt';
-$ip = $_SERVER['REMOTE_ADDR'];
-$date = date('Y-m-d H:i:s');
-$entry = "$date - $ip\n";
-
-file_put_contents($file, $entry, FILE_APPEND);
-?>
--- minecraft_status.php
+++
@@ -1,5 +0,0 @@
-<?php
-$url = 'https://api.mcsrvstat.us/2/bonkers-rasp.duckdns.org';
-$response = file_get_contents($url);
-echo $response;
-?>
